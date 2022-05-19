333fd1b3da6a3b1aef654a9cb2e05c89065b9d74
Daniel Quintero <dquintero.ing@gmail.com>
2022-05-19 00:50:21 -0500

    model user

--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  first_name: { type: String, default: null },
-  last_name: { type: String, default: null },
-  email: { type: String, unique: true },
-  password: { type: String },
-  status: { type: Boolean },
+  first_name: { type: String, required: [true, 'Please enter a first_name'] },
+  last_name: { type: String, required: [true, 'Please enter a last_name'] },
+  email: { type: String, required: [true, 'Please enter a email'] },
+  password: { type: String, required: [true, 'Please enter a status'] },
+  status: { type: Boolean, default: true },
   createdAt: { type: Date, default: new Date() },
   updatedAt: { type: Date, default: new Date() },
   token: { type: String },
